88d81326daf61d2b2e625781e7535dd4fef5c786
luccassousa-oss <luccas.sousa@aluno.senai.br>
2026-03-02 10:06:57 -0300

    commit

--- JS/login.js
+++ JS/login.js
@@ -1,69 +1,75 @@
 const form = document.getElementById("form");
-const username = document.getElementById("username")
-const password= document.getElementById("password");
-
+const username = document.getElementById("username");
+const password = document.getElementById("password");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
   checkForm();
-})
-
-
-
+});
 
 username.addEventListener("blur", () => {
   checkInputUsername();
-})
+});
 
+password.addEventListener("blur", () => {
+  checkInputPassword();
+});
 
-function checkInputUsername(){
+function checkInputUsername() {
   const usernameValue = username.value;
 
-  if(usernameValue === ""){
-    errorInput(username, "Preencha o username!")
-  }else{
+  if (usernameValue === "") {
+    errorInput(username, "Preencha o username!");
+  } else {
     const formItem = username.parentElement;
-    formItem.className = "form-content"
+    formItem.className = "form-content";
   }
 }
 
-function checkInputPassword(){
+function checkInputPassword() {
   const passwordValue = password.value;
-  if(passwordValue === ""){
-    errorInput(password, "A senha é obrigatória.")
-  }else if(passwordValue.length < 7){
-    errorInput(password, "A senha precisa ter no mínimo 6 caracteres.")
-  }else{
+
+  if (passwordValue === "") {
+    errorInput(password, "A senha é obrigatória.");
+  } else if (passwordValue.length < 6) {
+    errorInput(password, "A senha precisa ter no mínimo 6 caracteres.");
+  } else {
     const formItem = password.parentElement;
-    formItem.className = "form-content"
+    formItem.className = "form-content";
   }
-  
 }
 
-function checkForm(){
+function checkForm() {
   checkInputUsername();
   checkInputPassword();
 
-  const formItems = form.querySelectorAll(".form-content")
+  const formItems = form.querySelectorAll(".form-content");
 
-  const isValid = [...formItems].every( (item) => {
-    return item.className === "form-content"
+  const isValid = [...formItems].every((item) => {
+    return item.className === "form-content";
   });
 
-  if(isValid){
-    alert("Entrar")
+  if (isValid) {
+    login();
   }
-
 }
 
+function login() {
+  const senha = password.value;
 
-function errorInput(input, message){
+  if (senha === "111111") {
+    window.location.href = "homeadm.html";
+  } else if (senha === "000000") {
+    window.location.href = "homefunc.html";
+  } else {
+    alert("Senha incorreta!");
+  }
+}
+
+function errorInput(input, message) {
   const formItem = input.parentElement;
-  const textMessage = formItem.querySelector("a")
+  const textMessage = formItem.querySelector("a");
 
   textMessage.innerText = message;
-
-  formItem.className = "form-content error"
-
+  formItem.className = "form-content error";
 }
