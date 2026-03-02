a1900f20084870aeb505d2c01c20a91259039f26
damariscarmo-del <damaris.carmo@aluno.senai.br>
2026-03-02 08:43:12 -0300

    atualizaçoes

---
+++ JS/enviopedido.js
@@ -0,0 +1,11 @@
+fetch('', {
+method: 'POST',
+headers: {
+'Content-Type': 'application/json',
+},
+body: JSON.stringify({
+name: 'Nathan Sebhastian',
+email: 'ns@mail.com'
+}),
+}).then(response => response.json())
+.then(data => console.log(data))
