6bfdb48f422c383355dfb8d5d2d5b9ca330f0ec7
Lays-Pixel <lays.r.oliveira6@aluno.senai.br>
2026-06-15 08:32:57 -0300

    atualizações (cadprodutos funcionando)

--- JS/cadastrodeprateleira.js
+++ JS/cadastrodeprateleira.js
@@ -30,54 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
             data: data
         };
 
-    
-const myForm1 = document.getElementById('novaprateleira');
-if (myForm1 != null) {
-myForm1.addEventListener('submit', function (event) {
-    // 1. Prevenir o recarregamento da página ao submeter form
-    event.preventDefault();
-
-    fetch('https://localhost:7142/Prateleira', {
-        method: 'POST', //Para outros métodos, basta alterar aqui. Obs: Delete remove a parte do body e headers, e no get é conforme todos os exemploes feitos na Unidade interação com API 
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            Data_recebimento: document.getElementById("Data_recebimento").value   
-        }),
-    }).then(response => {
-        console.log(response);
-        if (response.status ==401){
-            alert ("Faça login antes de cadastrar!"); 
-            window.location.href="login.html";
-        }
-        response.json();})  
-        .then(data => {
-            console.log(data);        
-        })
-});
-}
-    function editaEntrega (idEntrega){
-        fetch('https://localhost:7142/Prateleira'+idEntrega, {
-            method: 'PUT',   
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ 
-                Data_recebimento: document.getElementById("Data_recebimento"+idEntrega).value
-            }),
-        }).then(response => {
-            if (response.status ==401){
-                alert ("Faça login antes de editar!");
-                window.location.href="login.html";
-            }else{
-                alert ("Entrega editada!");
-            }})
-           
-    }
-
     });
 
 });
--- JS/capratileira.js
+++ JS/capratileira.js
@@ -12,15 +12,54 @@ function cadastrar() {
     }
 }
 
-fetch('', {
-method: 'POST',
-headers: {
-'Content-Type': 'application/json',  
-},
-body: JSON.stringify({
-capdasprat: 'capacidade',
-identificação: 'identificação',
-status: 'stts'
-}),
-}).then(response => response.json())
-.then(data => console.log(data))
+    const myForm1 = document.getElementById('novaprateleira');
+if (myForm1 != null) {
+myForm1.addEventListener('submit', function (event) {
+    // 1. Prevenir o recarregamento da página ao submeter form
+    event.preventDefault();
+
+    fetch('https://localhost:7142/Prateleira', {
+        method: 'POST', //Para outros métodos, basta alterar aqui. Obs: Delete remove a parte do body e headers, e no get é conforme todos os exemploes feitos na Unidade interação com API 
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            capacidade: document.getElementById("capacidade").value,
+            status: document.getElementById("stts").value,
+            identificação: document.getElementById("identificação").value,   
+
+        }),
+    }).then(response => {
+        console.log(response);
+        if (response.status ==401){
+            alert ("Faça login antes de cadastrar!"); 
+            window.location.href="login.html";
+        }
+        response.json();})  
+        .then(data => {
+            console.log(data);        
+        })
+});
+} 
+    function editaPrateleira (idPrateleira){
+        fetch('https://localhost:7142/Prateleira'+idPrateleira, {
+            method: 'PUT',   
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+               capacidade: document.getElementById("capacidade"+idPrateleira).value,
+               status: document.getElementById("stts"+idPrateleira).value,
+               identificação: document.getElementById("identificação"+idPrateleira).value
+            }),
+        }).then(response => {
+            if (response.status ==401){
+                alert ("Faça login antes de editar!");
+                window.location.href="login.html";
+            }else{
+                alert ("Prateleira editada!");
+            }})
+           
+    }
